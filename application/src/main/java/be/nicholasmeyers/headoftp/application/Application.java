a518478bd917ea0bd5b553b6fb111f3686a01220
Nicholas Meyers <nicholas.meyers@hotmail.com>
2025-05-26 03:06:58 +0200

    chore: delete unused dependency

--- application/src/main/java/be/nicholasmeyers/headoftp/application/Application.java
+++ application/src/main/java/be/nicholasmeyers/headoftp/application/Application.java
@@ -4,16 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"be.nicholasmeyers.headoftp.route.adapter", "be.nicholasmeyers.headoftp.device.adapter"})
-@EnableJpaRepositories(basePackages = {"be.nicholasmeyers.headoftp.route.adapter.repository", "be.nicholasmeyers.headoftp.device.adapter.repository"})
-@EntityScan(basePackages = {"be.nicholasmeyers.headoftp.route.adapter.repository", "be.nicholasmeyers.headoftp.device.adapter.repository"})
-@EnableJpaAuditing
-@EnableScheduling
+//@ComponentScan(basePackages = {"be.nicholasmeyers.headoftp.route.adapter", "be.nicholasmeyers.headoftp.device.adapter"})
+//@EnableJpaRepositories(basePackages = {"be.nicholasmeyers.headoftp.route.adapter.repository", "be.nicholasmeyers.headoftp.device.adapter.repository"})
+//@EntityScan(basePackages = {"be.nicholasmeyers.headoftp.route.adapter.repository", "be.nicholasmeyers.headoftp.device.adapter.repository"})
+//@EnableJpaAuditing
+//@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
