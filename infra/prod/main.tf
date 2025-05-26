0ba3a796e12bed5ee157b73aadbf0a0ca84672d1
Nicholas Meyers <nicholas.meyers@hotmail.com>
2025-05-26 02:29:54 +0200

    chore: deploy application

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -3,6 +3,16 @@ module "vault" {
   vault_path = var.name
 }
 
+module "application" {
+  source           = "git::https://github.com/nicholasM95/terraform-modules.git//modules/k8s-helm-release?ref=v1.8.20"
+  image_tag        = var.image_tag
+  application_name = var.name
+  namespace_name   = var.namespace
+  helm_path        = "../../helm/application"
+  docker_config    = var.docker_config
+  vault_uri        = var.vault_uri
+}
+
 module "vault_connection" {
   depends_on                = [module.vault]
   source                    = "git::https://github.com/nicholasM95/terraform-modules.git//modules/vault-k8s?ref=v1.8.20"
