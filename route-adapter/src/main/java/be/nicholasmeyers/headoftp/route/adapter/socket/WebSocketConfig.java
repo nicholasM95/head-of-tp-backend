831541f3e1cf6d253d355ec8b5b30b91740d399c
Nicholas Meyers <nicholas.meyers@hotmail.com>
2025-05-29 12:05:02 +0200

    chore: update allowed origins web sockets

--- route-adapter/src/main/java/be/nicholasmeyers/headoftp/route/adapter/socket/WebSocketConfig.java
+++ route-adapter/src/main/java/be/nicholasmeyers/headoftp/route/adapter/socket/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/route-websocket").setAllowedOrigins("https://headoftp.com").withSockJS();
+        registry.addEndpoint("/route-websocket").setAllowedOrigins("https://headoftp.com", "http://localhost:5173").withSockJS();
     }
 }
