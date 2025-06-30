3a68006a101a7d9e85ce4c36efbd67ba9ff927fc
Nicholas Meyers <nicholas.meyers@hotmail.comxit>
2025-06-30 19:29:23 +0200

    chore: remove unused terraform vars

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -16,8 +16,6 @@ module "vault_connection" {
   depends_on                = [module.vault]
   source                    = "git::https://github.com/nicholasM95/terraform-modules.git//modules/vault-k8s?ref=v1.10.9"
   vault_path                = var.name
-  kubernetes_ca_cert        = var.kubernetes_ca_cert
-  kubernetes_internal_host  = var.kubernetes_internal_host
   service_account_name      = var.name
   service_account_namespace = var.namespace
 }
